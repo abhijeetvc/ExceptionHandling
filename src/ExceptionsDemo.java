e4113ff6be7665d834839ba37260c3ceb64803b5
abhi <abhijeetvc7@gmail.com>
2020-06-08 10:51:26 +0530

    Initial commit

---
+++ src/ExceptionsDemo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ExceptionsDemo {
+    
+    public static void main(String[] args){
+      //  System.out.println("Args : "+args[0]);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no1 :");
+        int a=sc.nextInt();
+        System.out.println("Enter no2");
+        int b=sc.nextInt();
+        int c=0;
+        try {
+            System.out.println("Args : "+args[0]);  //Array Index out of bounds exception
+            c = a / b;   // Divide by zero - Arithmetic exception
+            System.out.println("Print value of c: ");
+        }catch(Exception e){
+          //  System.out.println(e);
+            System.out.println("Denominator value cannot be zero. "+e);
+        }
+
+        System.out.println("Total : "+c);
+
+    }
+}
+
+//class Demo{
+//    void getData(){
+//        System.out.println("Hello");
+//    }
+//}
